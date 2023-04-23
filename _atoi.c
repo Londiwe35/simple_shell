c628eace454a2d0e07639a33dd03cc217789eced
SirJay88 <moledijustice@gmail.com>
2023-04-23 09:53:07 -0700

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -8,20 +8,20 @@
  */
 int interactive(info_t *info)
 {
-return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter of a total
- * @c: the character to check
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
-while (*delim)
-if (*delim++ == c)
-	return (1);
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
 	return (0);
 }
 
@@ -34,9 +34,9 @@ if (*delim++ == c)
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	return (1);
+		return (1);
 	else
-return (0);
+		return (0);
 }
 
 /**
@@ -53,9 +53,9 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
-	sign *= -1;
+			sign *= -1;
 
-	if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
